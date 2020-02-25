21049468324685dd5f5c36a4f5109f03369ced4d
Kenneth Guo <kenneth.guo@aurtra.net>
2020-02-25 18:07:30 +1000

    added signup page and login page UI

--- frontend/src/components/login/login.jsx
+++ frontend/src/components/login/login.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './login.css';
+import { Link } from 'react-router-dom';
+import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
 
 export class LoginComponent extends React.Component {
     constructor(props) {
@@ -9,7 +11,37 @@ export class LoginComponent extends React.Component {
 
     render() {
         return (
-            <div>Login page works!</div>
+            <Grid textAlign='center' className="grid" verticalAlign='middle'>
+                <Grid.Column className="grid-column">
+                    <Header as='h2' color='teal' textAlign='center'>
+                        Log-in to your account
+                    </Header>
+                    <Form size='large'>
+                        <Segment>
+                            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
+                            <Form.Input
+                                fluid
+                                icon='lock'
+                                iconPosition='left'
+                                placeholder='Password'
+                                type='password'
+                            />
+
+                            <Button color='teal' fluid size='large'>
+                                Login
+                            </Button>
+                        </Segment>
+                    </Form>
+                    <Link to='/signup'>
+                        <Message>
+                            New to us? Sign Up
+                        </Message>
+                    </Link>
+                    <Message>
+                        Forgot your password?
+                    </Message>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
--- frontend/src/components/signup/signup.jsx
+++ frontend/src/components/signup/signup.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './signup.css';
+import { Button, Form, Grid, Header, Segment } from 'semantic-ui-react'
 
 export class SignupComponent extends React.Component {
     constructor(props) {
@@ -9,7 +10,22 @@ export class SignupComponent extends React.Component {
 
     render() {
         return (
-            <div>Signup page works!</div>
+            <Grid textAlign='center' className="grid" verticalAlign='middle'>
+                <Grid.Column className="grid-column">
+                    <Header as='h2' color='teal' textAlign='center'>
+                        Sign up
+                    </Header>
+                    <Form size='large'>
+                        <Segment>
+                            <Form.Input fluid icon='user' iconPosition='left' placeholder='Name' />
+                            <Form.Input fluid icon='mail' iconPosition='left' placeholder='Email' />
+                            <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password (6 characters minimum)' type='password'/>
+                            <Form.Input fluid icon='lock' iconPosition='left' placeholder='Password Confirm' type='password'/>
+                            <Button color='teal' fluid size='large'>Submit</Button>
+                        </Segment>
+                    </Form>
+                </Grid.Column>
+            </Grid>
         )
     }
 }
